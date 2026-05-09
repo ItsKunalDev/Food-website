392203576e6bf160d52dd23ebf4f79498c7fec31
Kunal Dev <kunaldev9142@gmail.com>
2026-05-09 17:15:19 +0530

    fix: restore hardcoded firebase config

--- firebase.js
+++ firebase.js
@@ -13,13 +13,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAQBXVoMpJFjQd8JgkYpMMbLFDtVxod-mk",
+    authDomain: "spiceroutes-434fd.firebaseapp.com",
+    projectId: "spiceroutes-434fd",
+    storageBucket: "spiceroutes-434fd.firebasestorage.app",
+    messagingSenderId: "447303129354",
+    appId: "1:447303129354:web:d1d1f57765088f98db82f3",
+    measurementId: "G-G1DMKRHZJ8"
 };
 
 const app = initializeApp(firebaseConfig);
